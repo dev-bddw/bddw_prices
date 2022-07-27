aff70f5552b2b4f1adcdcb3a74d0b8623d3a5602
Lance Knickerbocker <lance@bddw.com>
2022-07-27 10:42:38 -0400

    made migrations for rule_display_1 change

--- price_records/models.py
+++ price_records/models.py
@@ -188,6 +188,7 @@ class FormulaPriceRecord(models.Model):
     inset = models.IntegerField(null=True, blank=True)
 
     rule_display_1 = models.CharField(
+        default="",
         blank=False,
         null=False,
         help_text="Will convert [LENGTH] L x [DEPTH] D or you can just manually ",
