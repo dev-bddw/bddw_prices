817e5f34a2afde2a2992df9ff022ea5fa87b4742
lance <lance@bddw.com>
2023-01-30 16:06:05 -0500

    improved the helpers functions for tear_sheets

--- tear_sheets/helpers.py
+++ tear_sheets/helpers.py
@@ -4,20 +4,27 @@ from products.models import CatSeriesItem
 from .models import TearSheet, TearSheetDetail
 
 
-def return_price_records_by_rule_type(tear_sheet_pk):
-    tear_sheet = TearSheet.objects.get(pk=tear_sheet_pk)
-    category_series_items = CatSeriesItem.objects.filter(tear_sheet=tear_sheet)
+def return_price_records_by_rule_type(pk:int):
+    '''
+    lookup all pricerecords using tearsheet pk
+    organize into dict by rule type
+    '''
+    category_series_items = CatSeriesItem.objects.filter(tear_sheet=pk)
 
     if len(category_series_items) != 0:
 
         list_of_price_records = PriceRecord.objects.filter(
             cat_series_item__in=category_series_items
         )
+        
         rule_types = []
+
         for x in list_of_price_records:
             if x.rule_type not in rule_types:
                 rule_types.append(x.rule_type)
+
         price_records = []
+       
         for p in rule_types:
             price_records.append(
                 {f"{p}": [x for x in list_of_price_records if x.rule_type == p]}
@@ -28,13 +35,27 @@ def return_price_records_by_rule_type(tear_sheet_pk):
         return None
 
 
-def return_details_by_title(tear_sheet_pk):
-    details = TearSheetDetail.objects.filter(tear_sheet__pk=tear_sheet_pk)
-    names = []
-    for x in details:
-        if x.name not in names:
-            names.append(x.name)
-    detail_records = []
-    for p in names:
-        detail_records.append({f"{p}": [x for x in details if x.name == p]})
-    return detail_records
+def return_details_by_title(pk: int):
+    '''
+    lookup all detail records by tearsheek pk 
+    organize into dict by title 
+    '''
+      
+    details = TearSheetDetail.objects.filter(tear_sheet__pk=pk)
+    
+    if len(details) != 0:
+            
+        names = []
+
+        for x in details:
+            if x.name not in names:
+                names.append(x.name)
+        detail_records = []
+        for p in names:
+            detail_records.append({f"{p}": [x for x in details if x.name == p]})
+        
+        return detail_records
+
+    else:
+
+        return None
