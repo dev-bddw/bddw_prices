e1d841a48a793df6fd0bc9912a1ea57d0a7b412c
lance <lance@freelancelance.com>
2023-05-25 08:37:06 -0400

    updated search

--- react_views/r_search/templates/r_search/search/src/App.jsx
+++ react_views/r_search/templates/r_search/search/src/App.jsx
@@ -2,7 +2,7 @@ import {useRef, useState, useEffect} from 'react'
 
 function App() {
 
-  const [search, setSearch] = useState('search')
+  const [search, setSearch] = useState('')
   const [tearsheets, setTearsheets] = useState(CONTEXT.tearsheets)
 
   function onChangeHandler(event) {
@@ -42,21 +42,26 @@ function App() {
 						})
 	}
 
+  const logo = () => {
+    return(
+      <img style={{height:'300px'}} className="item-center" src={CONTEXT.logo_url}/>
+     )
+  }
 
   return (
     <div className="container px-5">
     <div className="grid grid cols-1 justify-center">
-      <h1 style={{'font-size': '30px'}}>SEARCH ALL TEARSHEETS</h1>
-      <p>search all tearsheets by name</p>
-      <input style={{width: "300px"}} className="shadow appearance-none border rounded py-2 px-1 my-5 text-gray-700 leading-tight focus:outline-none focus:shadow-outliney-1" type='text' onChange={ (event)=> onChangeHandler(event)} value={search}></input>
-      <div className="text-left py-3 text-xs">Displaying {tearsheets.length} results...</div>
-      <div>
-          <table className="border w-1200 text-sm text-left text-gray-500">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+      <span style={{'letter-spacing':'.75rem', 'font-weight':'lighter', 'font-size':'40px'}}>SEARCH</span>
+      <input style={{width: "300px"}} className="shadow appearance-none border rounded py-2 px-1 my-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outliney-1" type='text' onChange={ (event)=> onChangeHandler(event)} value={search}></input>
+      <div className="text-start py-3 text-gray-300 text-xs">Displaying {tearsheets.length} results...</div>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="border rounded w-1200 text-sm text-left text-gray-500">
+              <thead className="text-xs text-gray-400 uppercase bg-gray-50">
               <tr>
-                <th className="px-2">type</th>
-                <th className="px-2">tearsheet name</th>
-                <th className="px-2">updated on</th>
+                <th className="py-3 px-2">type</th>
+                <th className="py-3 px-2"></th>
+                <th className="py-3 px-2"></th>
+                <th className="py-3 px-2">updated on</th>
               </tr>
              </thead>
               <tbody>
@@ -65,21 +70,24 @@ function App() {
               <td style={{width: '150px'}} className="px-2">
                no results
               </td>
-              <td style={{width: '500px'}} className="px-2  font-medium text-gray-900 whitespace-nowrap">
+              <td style={{width: '500px'}} className="px-2 font-medium text-gray-900 whitespace-nowrap">
               </td>
               <td style={{width: '300px'}} className="px-2 ">
               </td>
             </tr>
     :
                tearsheets.map( (tearsheet) => { return(
-                  <tr className="bg-white">
-                     <td style={{width: '150px'}} className="px-2 ">
+                  <tr className="border-b bg-white">
+                     <td style={{width: '150px'}} className="py-2 px-2 ">
                         {tearsheet.type}
                       </td>
-                      <td style={{width:'500px'}} className="px-2 font-medium text-gray-900 whitespace-nowrap">
-                          <a style={{'color': 'black'}} href={tearsheet.url} className="font-medium text-blue-600 hover:underline">{tearsheet.title}</a>
+                      <td>
+                        <img className="px-1 text-gray-500" style={{height:'15px'}} src={tearsheet.image}/>
                       </td>
-                      <td style={{width: '300px'}} className="px-2 ">
+                      <td style={{width:'500px'}} className="py-2 px-2 font-medium text-gray-500 whitespace-nowrap">
+                          <a href={tearsheet.url} className="hover:underline">{tearsheet.title}</a>
+                      </td>
+                      <td style={{width: '300px'}} className="py-2 px-2 ">
                         {tearsheet.updated_on}
                       </td>
                   </tr>
