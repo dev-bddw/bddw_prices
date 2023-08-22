2f531e38cdde3e6ea6ab7a7e3a787a9540b142dc
Lance Knickerbocker <lance-rlf@dev-macbook.local>
2023-08-22 09:28:11 -0400

    problem: the bash scripting for building these javascript frontend .js files doesn't make sense on mac os / the paths aren't relative to the dir structure as they should be solution: edit the bash scripts to be relative

--- react_views/build_all.sh
+++ react_views/build_all.sh
@@ -4,24 +4,25 @@
 # react js and react css are in the correct template position
 # then run run_static.sh to apply static template tags to the
 # react templates (index.html)
-cd /home/lance/dev/django-projects/bddw_prices/react_views/
-./r_tear_sheets/templates/edit/build.sh
-./r_tear_sheets/templates/detail/build.sh
-./r_tear_sheets/templates/detail_for_print/build.sh
-./r_tear_sheets/templates/detail_for_print_list/build.sh
-./r_formula_tear_sheets/templates/form_edit/build.sh
-./r_formula_tear_sheets/templates/form_detail/build.sh
-./r_formula_tear_sheets/templates/form_detail_for_print/build.sh
-./r_formula_tear_sheets/templates/form_detail_for_print_list/build.sh
-./r_formula_tear_sheets/r_form_gbp/templates/form_gbp_edit/build.sh
-./r_formula_tear_sheets/r_form_gbp/templates/form_gbp_detail/build.sh
-./r_formula_tear_sheets/r_form_gbp/templates/form_gbp_detail_for_print/build.sh
-./r_formula_tear_sheets/r_form_gbp/templates/form_gbp_detail_for_print_list/build.sh
-./r_tear_sheets/r_gbp/templates/gbp_detail/build.sh
-./r_tear_sheets/r_gbp/templates/gbp_edit/build.sh
-./r_tear_sheets/r_gbp/templates/gbp_detail_for_print/build.sh
-./r_tear_sheets/r_gbp/templates/gbp_detail_for_print_list/build.sh
-./r_search/templates/r_search/search/build.sh
-./r_price_lists/templates/r_price_lists/pricelist/build.sh
-# run static
-/home/lance/dev/django-projects/bddw_prices/react_views/staticfy.py
+echo $PWD
+# cd /home/lance/dev/django-projects/bddw_prices/react_views/
+./react_views/r_tear_sheets/templates/edit/build.sh
+./react_views/r_tear_sheets/templates/detail/build.sh
+./react_views/r_tear_sheets/templates/detail_for_print/build.sh
+./react_views/r_tear_sheets/templates/detail_for_print_list/build.sh
+./react_views/r_formula_tear_sheets/templates/form_edit/build.sh
+./react_views/r_formula_tear_sheets/templates/form_detail/build.sh
+./react_views/r_formula_tear_sheets/templates/form_detail_for_print/build.sh
+./react_views/r_formula_tear_sheets/templates/form_detail_for_print_list/build.sh
+./react_views/r_formula_tear_sheets/r_form_gbp/templates/form_gbp_edit/build.sh
+./react_views/r_formula_tear_sheets/r_form_gbp/templates/form_gbp_detail/build.sh
+./react_views/r_formula_tear_sheets/r_form_gbp/templates/form_gbp_detail_for_print/build.sh
+./react_views/r_formula_tear_sheets/r_form_gbp/templates/form_gbp_detail_for_print_list/build.sh
+./react_views/r_tear_sheets/r_gbp/templates/gbp_detail/build.sh
+./react_views/r_tear_sheets/r_gbp/templates/gbp_edit/build.sh
+./react_views/r_tear_sheets/r_gbp/templates/gbp_detail_for_print/build.sh
+./react_views/r_tear_sheets/r_gbp/templates/gbp_detail_for_print_list/build.sh
+./react_views/r_search/templates/r_search/search/build.sh
+./react_views/r_price_lists/templates/r_price_lists/pricelist/build.sh
+./react_views/r_price_lists/templates/r_price_lists/pricelist_print/build.sh
+./react_views/r_price_lists/templates/r_price_lists/pricelist_print_gbp/build.sh
