05d280ce79f4c7e99c880efb53028de6576db5f8
Lance Knickerbocker <lance-rlf@dev-macbook.local>
2023-08-22 11:32:44 -0400

    problem: there was a bug in the build_all.sh related to the pricelist scripts, which have a different configuration than the others (it has a parent level script that builds all three templates) solution: corrected the build_all.sh so that it points only to that script

--- react_views/build_all.sh
+++ react_views/build_all.sh
@@ -4,7 +4,10 @@
 # react js and react css are in the correct template position
 # then run run_static.sh to apply static template tags to the
 # react templates (index.html)
-echo $PWD
+#
+# DOES NOT WORK ON MAC OS
+#
+#
 # cd /home/lance/dev/django-projects/bddw_prices/react_views/
 ./react_views/r_tear_sheets/templates/edit/build.sh
 ./react_views/r_tear_sheets/templates/detail/build.sh
@@ -23,6 +26,4 @@ echo $PWD
 ./react_views/r_tear_sheets/r_gbp/templates/gbp_detail_for_print/build.sh
 ./react_views/r_tear_sheets/r_gbp/templates/gbp_detail_for_print_list/build.sh
 ./react_views/r_search/templates/r_search/search/build.sh
-./react_views/r_price_lists/templates/r_price_lists/pricelist/build.sh
-./react_views/r_price_lists/templates/r_price_lists/pricelist_print/build.sh
-./react_views/r_price_lists/templates/r_price_lists/pricelist_print_gbp/build.sh
+./react_views/r_price_lists/templates/r_price_lists/build.sh
