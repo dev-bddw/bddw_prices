6c52f692f0efb00bdb15ee0904adb7c5a62ca323
lance <lance@system76-pc.localdomain>
2023-08-31 15:32:47 -0400

    problem: there was no way to quickly install all the dependencies for each react app (each view) solution: there's a nifty command line --prefix you can do with npm that allows you to specify an install dir, quickly made an install.sh that runs npm install with this prefix and once run you can now run buildall

---
+++ react_views/install_all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# helpful script to install all the dependencies for building node projects
+#
+# DOES NOT WORK ON MAC OS
+#
+#
+# cd /home/lance/dev/django-projects/bddw_prices/react_views/
+npm install --prefix ./react_views/r_tear_sheets/templates/edit/
+npm install --prefix ./react_views/r_tear_sheets/templates/detail/
+npm install --prefix ./react_views/r_tear_sheets/templates/detail_for_print/
+npm install --prefix ./react_views/r_tear_sheets/templates/detail_for_print_list/
+npm install --prefix ./react_views/r_formula_tear_sheets/templates/form_edit/
+npm install --prefix ./react_views/r_formula_tear_sheets/templates/form_detail/
+npm install --prefix ./react_views/r_formula_tear_sheets/templates/form_detail_for_print/
+npm install --prefix ./react_views/r_formula_tear_sheets/templates/form_detail_for_print_list/
+npm install --prefix ./react_views/r_formula_tear_sheets/r_form_gbp/templates/form_gbp_edit/
+npm install --prefix ./react_views/r_formula_tear_sheets/r_form_gbp/templates/form_gbp_detail/
+npm install --prefix ./react_views/r_formula_tear_sheets/r_form_gbp/templates/form_gbp_detail_for_print/
+npm install --prefix ./react_views/r_formula_tear_sheets/r_form_gbp/templates/form_gbp_detail_for_print_list/
+npm install --prefix ./react_views/r_tear_sheets/r_gbp/templates/gbp_detail/
+npm install --prefix ./react_views/r_tear_sheets/r_gbp/templates/gbp_edit/
+npm install --prefix ./react_views/r_tear_sheets/r_gbp/templates/gbp_detail_for_print/
+npm install --prefix ./react_views/r_tear_sheets/r_gbp/templates/gbp_detail_for_print_list/
+npm install --prefix ./react_views/r_search/templates/r_search/search/
+npm install --prefix ./react_views/r_price_lists/templates/r_price_lists/pricelist/
+npm install --prefix ./react_views/r_price_lists/templates/r_price_lists/pricelist_print/
+npm install --prefix ./react_views/r_price_lists/templates/r_price_lists/pricelist_print_gbp/
